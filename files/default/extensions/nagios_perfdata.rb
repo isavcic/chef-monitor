558a0e52bda6652b2c8f2d9be75a8fc14a6fde9d
Ivan Savcic <ivans@vast.com>
2015-03-18 15:03:41 +0100

    Output sanitization

--- files/default/extensions/nagios_perfdata.rb
+++ files/default/extensions/nagios_perfdata.rb
@@ -23,21 +23,29 @@ module Sensu
         client = event[:client]
         check  = event[:check]
 
-        # https://www.nagios-plugins.org/doc/guidelines.html#AEN200
-        perfdata = check[:output].split('|').last.strip
+        # I can has perfdata?
+        if check[:output].include? '|'
+          # https://www.nagios-plugins.org/doc/guidelines.html#AEN200
+          perfdata = check[:output].split('|').last.strip
 
-        perfdata.split(/\s+/).each do |data|
-          # label=value[UOM];[warn];[crit];[min];[max]
-          label, value = data.split('=')
+          perfdata.split(/\s+/).each do |data|
+            # label=value[UOM];[warn];[crit];[min];[max]
+            label, value = data.split('=')
 
-          name = label.strip.gsub(/\W/, '_')
-          measurement = value.strip.split(';').first.gsub(/[^-\d\.]/, '')
+            # Removing leading, trailing and multiple underlines
+            name = label.strip.gsub(/\W/, '_').gsub(/^_+/, '').gsub(/_+$/, '').gsub(/_+/, '_')
+            measurement = value.strip.split(';').first.gsub(/[^-\d\.]/, '')
 
-          path = [client[:name], check[:name], name].join('.')
+            # We don't want dots in client name
+            path = [client[:name].gsub('.', '-'), check[:name], name].join('.')
 
-          result << [path, measurement, check[:executed]].join("\t")
+            result << [path, measurement, check[:executed]].join("\t")
+          end
+          yield(result.join("\n") + "\n", 0)
+        else
+          # No perfdata, bailing out
+          yield("\n", 0)
         end
-        yield(result.join("\n") + "\n", 0)
       end
     end
   end
